92b0fff847e85813b82fe755e5ed7dd9e51f16d7
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-11-12 14:21:21 +0000

    bug in nc computation

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ def compute_gini(x):
 def compute_nc(blocks_per_pool):
     nc = [0, 0]
     for (name, blocks) in sorted(blocks_per_pool.items(), key=lambda x: x[1], reverse=True):
-        if nc[1] < 0.5:
+        if nc[1] < 50:
             nc[0] += 1
             nc[1] += 100 * blocks / sum([i[1] for i in blocks_per_pool.items()])
         else:
