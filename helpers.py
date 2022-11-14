4b240c719efa5c98e306e3b8a4b2823a49486975
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-11-14 12:17:42 +0000

    use faster gini computation

--- helpers.py
+++ helpers.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 
-def compute_gini(x):
-    mad = np.abs(np.subtract.outer(x, x)).mean()
-    rmad = mad/np.mean(x)
-    return 0.5 * rmad
+def compute_gini(lst):
+    array = np.array(lst)
+
+    """
+    Calculate the Gini coefficient of a numpy array.
+    Source: https://github.com/oliviaguest/gini
+    """
+    array = array.flatten()
+    if np.amin(array) < 0:
+        # Values cannot be negative:
+        array -= np.amin(array)
+    array = np.sort(array)
+    index = np.arange(1,array.shape[0]+1)
+    n = array.shape[0]
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
 
 
 def compute_nc(blocks_per_pool):
