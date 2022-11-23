da5557d7ddadfe2ff0052c9a8487f34cf8103ec8
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-11-23 16:41:12 +0000

    update bitcoin parser

--- parsers/bitcoin.py
+++ parsers/bitcoin.py
@@ -1,14 +1,12 @@
 import json
 
+with open('raw_data.json') as f:
+    data = json.load(f)
+    data = sorted(data, key=lambda x: x['number'])
 
-def parse(project_dir):
-    with open(project_dir + '/data.json') as f:
-        data = json.load(f)
-        data = sorted(data, key=lambda x: x['number'])
+for tx in data:
+    tx['coinbase_addresses'] = ','.join(list(set([i['addresses'][0] for i in tx['outputs'] if (i['addresses'] and int(i['value']) > 0 and i['type'] != 'nonstandard')])))
+    del tx['outputs']
 
-    for tx in data:
-        tx['coinbase_addresses'] = list(set([i['addresses'][0] for i in tx['outputs'] if (i['addresses'] and int(i['value']) > 0 and i['type'] != 'nonstandard')]))
-        del tx['outputs']
-
-    with open(project_dir + '/parsed_data.json', 'w') as f:
-        f.write(json.dumps({'blocks': data}))
+with open('data.json', 'w') as f:
+    f.write(json.dumps({'blocks': data}, indent=4))
