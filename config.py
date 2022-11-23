d3a3bc728daab4da592285431041d3ec5d858e4a
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-11-23 18:47:29 +0000

    remove config file

--- config.py
+++
@@ -1,7 +0,0 @@
-RANGE = 7  # 0: all, 4: years, 7: months, 10: days
-POOL_CLUSTERING = False  # coinbase tags that identify pools
-LEGAL_LINKS = False  # known legal links (eg. parent company) between pools
-ADDRESS_LINKS = False  # consistently shared coinbase addresses between pools
-PRINT_DISTRIBUTION = False
-POOL_PIE = True
-TIME_SERIES_OUTPUT = True
