551e845c6f7d16102a1b7f0918f3573e8af59485
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-12-02 16:17:18 +0000

    include nonstandard bitcoin addresses in parser

--- parsers/bitcoin.py
+++ parsers/bitcoin.py
@@ -5,7 +5,7 @@ with open('raw_data.json') as f:
     data = sorted(data, key=lambda x: x['number'])
 
 for tx in data:
-    tx['coinbase_addresses'] = ','.join(list(set([i['addresses'][0] for i in tx['outputs'] if (i['addresses'] and int(i['value']) > 0 and i['type'] != 'nonstandard')])))
+    tx['coinbase_addresses'] = ','.join(list(set([i['addresses'][0] for i in tx['outputs'] if (i['addresses'] and int(i['value']) > 0)])))
     del tx['outputs']
 
 with open('data.json', 'w') as f:
