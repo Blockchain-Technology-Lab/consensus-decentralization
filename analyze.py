b78bf6481c2c0b60c1645f7970658ae041f14e1d
Dimitris Karakostas <dimit.karakostas@gmail.com>
2022-12-07 15:03:35 +0000

    refactor analyze code

--- analyze.py
+++ analyze.py
@@ -21,98 +21,88 @@ PROJECTS = [
     'zcash',
 ]
 
+
+def analyze_data(project_name, timeframe, yearly_entities):
+    blocks_per_entity = {}
+    with open('ledgers/{}/{}.csv'.format(project_name, timeframe)) as f:
+        for idx, line in enumerate(f.readlines()):
+            if idx > 0:
+                row = (','.join([i for i in line.split(',')[:-1]]), line.split(',')[-1])
+                blocks_per_entity[row[0]] = int(row[1])
+
+        if blocks_per_entity.keys():
+            for entity in yearly_entities[timeframe[:4]]:
+                if entity not in blocks_per_entity.keys():
+                    blocks_per_entity[entity] = 0
+
+            gini = compute_gini(blocks_per_entity)
+            nc = compute_nc(blocks_per_entity)
+            entropy = compute_entropy(blocks_per_entity)
+        else:
+            gini, nc, entropy = '', ('', ''), ''
+
+        return gini, nc, entropy
+
+
 def analyze(projects, timeframe_argument=False):
-    months = []
-    gini_series = defaultdict(list)
-    nc_series = defaultdict(list)
-    entropy_series = defaultdict(list)
+    gini_csv = {'0': 'timeframe'}
+    nc_csv = {'0': 'timeframe'}
+    entropy_csv = {'0': 'timeframe'}
+
     for project_name in projects:
-        project_dir = str(pathlib.Path(__file__).parent.resolve()) + '/ledgers/{}'.format(project_name)
+        gini_csv['0'] += ',' + project_name
+        nc_csv['0'] += ',' + project_name
+        entropy_csv['0'] += ',' + project_name
 
         yearly_entities = {}
         if timeframe_argument:
-            START_YEAR = int(timeframe_argument[:4])
-            END_YEAR = START_YEAR + 1
+            timeframes = [timeframe_argument]
+            start = int(timeframe_argument[:4])
+            end = start + 1
+        else:
+            timeframes = []
+            for year in range(START_YEAR, END_YEAR):
+                for month in range(1, 13):
+                    timeframes.append('{}-{}'.format(year, str(month).zfill(2)))
 
-        for idx, year in enumerate(range(START_YEAR, END_YEAR)):
-            yearly_entities[year] = set()
-            for month in range(1, 13):
-                timeframe = '{}-{}'.format(year, str(month).zfill(2))
-                if timeframe not in months:
-                    months.append(timeframe)
+        for timeframe in timeframes:
+            if timeframe not in gini_csv.keys():
+                gini_csv[timeframe] = timeframe
+                nc_csv[timeframe] = timeframe
+                entropy_csv[timeframe] = timeframe
+
+            year = timeframe[:4]
+            if year not in yearly_entities.keys():
+                yearly_entities[year] = set()
                 try:
-                    with open('ledgers/{}/{}.csv'.format(project_name, timeframe)) as f:
+                    with open('ledgers/{}/{}.csv'.format(project_name, year)) as f:
                         for idx, line in enumerate(f.readlines()):
                             if idx > 0:
                                 row = (','.join([i for i in line.split(',')[:-1]]), line.split(',')[-1])
                                 yearly_entities[year].add(row[0])
                 except FileNotFoundError:
+                    project_dir = str(pathlib.Path(__file__).parent.resolve()) + '/ledgers/{}'.format(project_name)
                     with open(project_dir + '/data.json') as f:
                         data = json.load(f)
-                    process(project_name, data, timeframe)
+                    process(project_name, data, year)
             
-            if timeframe_argument and len(timeframe_argument) > 4:
-                month_lst = [int(timeframe_argument[5:])]
+            gini, nc, entropy = analyze_data(project_name, timeframe, yearly_entities)
+
+            gini_csv[timeframe] += ',{}'.format(gini)
+            nc_csv[timeframe] += ',{}'.format(nc[0])
+            entropy_csv[timeframe] += ',{}'.format(entropy)
+
+            if gini != '':
+                print('[{0:12} {1:7}] \t Gini: {2:.6f}   NC: {3:3} ({4:.2f}%)   Entropy: {5:.6f}'.format(project_name, timeframe, gini, nc[0], nc[1], entropy))
             else:
-                month_lst = range(1, 13)
-
-            for month in month_lst:
-                timeframe = '{}-{}'.format(year, str(month).zfill(2))
-                blocks_per_entity = {}
-                with open('ledgers/{}/{}.csv'.format(project_name, timeframe)) as f:
-                    for idx, line in enumerate(f.readlines()):
-                        if idx > 0:
-                            row = (','.join([i for i in line.split(',')[:-1]]), line.split(',')[-1])
-                            blocks_per_entity[row[0]] = int(row[1])
-
-                    if blocks_per_entity.keys():
-                        for entity in yearly_entities[year]:
-                            if entity not in blocks_per_entity.keys():
-                                blocks_per_entity[entity] = 0
-
-                        gini = compute_gini(blocks_per_entity)
-                        nc = compute_nc(blocks_per_entity)
-                        entropy = compute_entropy(blocks_per_entity)
-                        print('[{0:12} {1:7}] \t Gini: {2:.6f}   NC: {3:3} ({4:.2f}%)   Entropy: {5:.6f}'.format(project_name, timeframe, gini, nc[0], nc[1], entropy))
-
-                        gini_series[project_name].append(gini)
-                        nc_series[project_name].append(nc[0])
-                        entropy_series[project_name].append(entropy)
-                    else:
-                        print('[{}, {}] No data'.format(project_name, timeframe))
-
-                        gini_series[project_name].append(0)
-                        nc_series[project_name].append(0)
-                        entropy_series[project_name].append(0)
-
-    gini_csv = ['Month']
-    nc_csv = ['Month']
-    entropy_csv = ['Month']
-    for month in months:
-        gini_csv.append(month)
-        nc_csv.append(month)
-        entropy_csv.append(month)
-
-    for i, project_name in enumerate(projects):
-        gini_csv[0] += ',' + project_name
-        for j, data in enumerate(gini_series[project_name]):
-            gini_csv[j+1] += ',{}'.format(data if data else '')
-
-        nc_csv[0] += ',' + project_name
-        for j, data in enumerate(nc_series[project_name]):
-            nc_csv[j+1] += ',{}'.format(data if data else '')
-
-        entropy_csv[0] += ',' + project_name
-        for j, data in enumerate(entropy_series[project_name]):
-            entropy_csv[j+1] += ',{}'.format(data if data else '')
+                print('[{}, {}] No data'.format(project_name, timeframe))
 
     with open('gini.csv', 'w') as f:
-        f.write('\n'.join(gini_csv))
+        f.write('\n'.join([i[1] for i in sorted(gini_csv.items(), key=lambda x: x[0])]))
     with open('nc.csv', 'w') as f:
-        f.write('\n'.join(nc_csv))
+        f.write('\n'.join([i[1] for i in sorted(nc_csv.items(), key=lambda x: x[0])]))
     with open('entropy.csv', 'w') as f:
-        f.write('\n'.join(entropy_csv))
-
+        f.write('\n'.join([i[1] for i in sorted(entropy_csv.items(), key=lambda x: x[0])]))
 
 if __name__ == '__main__':
     try:
