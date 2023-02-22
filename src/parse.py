de59eca8b663374f2d0db9194c474c2e0e817add
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-02-22 17:29:26 +0000

    Allow parse to be run directly from cmd

--- src/parse.py
+++ src/parse.py
@@ -1,3 +1,4 @@
+import argparse
 from src.parsers.default_parser import DefaultParser
 from src.parsers.cardano_parser import CardanoParser
 from src.parsers.dummy_parser import DummyParser
@@ -20,10 +21,30 @@ def parse(project, force_parse=False):
     """
     Parse raw data, unless already parsed
     :param project: the ledger whose data should be parsed
-    :param force_parse: if True, then raw data will be parsed, regardless of whether parsed data for the some or all of
-     the projects already exist
+    :param force_parse: if True, then raw data will be parsed, regardless of whether parsed data for the some or all of the projects already exist
     """
     parsed_data_file = OUTPUT_DIR / project / 'parsed_data.json'
     if force_parse or not parsed_data_file.is_file():
         parser = ledger_parser[project](project)
         parser.parse()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--ledger',
+        nargs="*",
+        type=str.lower,
+        default=None,
+        choices=[ledger for ledger in ledger_parser],
+        help='The ledgers that will be analyzed.'
+    )
+    parser.add_argument(
+        '--force-parse',
+        action='store_true',
+        help='Flag to specify whether to parse the raw data, regardless if the parsed data file exists.'
+    )
+    args = parser.parse_args()
+
+    parse(args.ledger, args.force_parse)
