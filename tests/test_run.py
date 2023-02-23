ee2325b71e1684f19f07d5617667653d341e9270
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-02-23 12:00:41 +0000

    Test input date validation

---
+++ tests/test_run.py
@@ -0,0 +1,31 @@
+import argparse
+import pytest
+from run import valid_date
+
+
+def test_valid_date():
+    for d in [
+        '2022',
+        '2022-01',
+        '2022-01-01',
+    ]:
+        assert valid_date(d)
+
+    for d in [
+        '2022-01-1',
+        '2022-1-1',
+        '2022-1-01',
+        '2022-1',
+        '2022/1/01',
+        '2022/01/1',
+        '2022/1',
+        '2022/01',
+        '2022.1.01',
+        '2022.01.1',
+        '2022.1',
+        '2022.01',
+        'blah'
+    ]:
+        with pytest.raises(argparse.ArgumentTypeError) as e_info:
+            valid_date(d)
+        assert e_info.type == argparse.ArgumentTypeError
