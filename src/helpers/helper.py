a5c2a6e4c1cfe8b2df455b0b388b155ba8d4e26f
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-02-28 15:55:52 +0000

    Update pool cluster and legal link parsing with new format

--- src/helpers/helper.py
+++ src/helpers/helper.py
@@ -1,36 +1,82 @@
 # module with helper functions
 import pathlib
 import json
+import datetime
+import calendar
 
 YEAR_DIGITS = 4
 INPUT_DIR = pathlib.Path(__file__).resolve().parent.parent.parent / 'input'
 OUTPUT_DIR = pathlib.Path(__file__).resolve().parent.parent.parent / 'output'
 
 
+def get_start_date(timeframe):
+    time_list = [int(i) for i in timeframe.split('-')]
+    if len(time_list) == 3:
+        start = datetime.date(time_list[0], time_list[1], time_list[2])
+    elif len(time_list) == 2:
+        start = datetime.date(time_list[0], time_list[1], 1)
+    elif len(time_list) == 1:
+        start = datetime.date(time_list[0], 1, 1)
+    return start
+
+
+def get_timeframe_end(timeframe):
+    time_list = [int(i) for i in timeframe.split('-')]
+    if len(time_list) == 3:
+        end = datetime.date(time_list[0], time_list[1], time_list[2])
+    elif len(time_list) == 2:
+        end = datetime.date(time_list[0], time_list[1], calendar.monthrange(time_list[0], time_list[1])[1])
+    elif len(time_list) == 1:
+        end = datetime.date(time_list[0], 12, 31)
+    return end
+
+
+def get_time_period(frm, to):
+    if frm:
+        start = get_start_date(frm)
+    else:
+        start = datetime.date(2000, 1, 1)
+
+    if to:
+        time_list = [int(i) for i in to.split('-')]
+        if len(time_list) == 3:
+            end = datetime.date(time_list[0], time_list[1], time_list[2])
+        elif len(time_list) == 2:
+            end = datetime.date(time_list[0], time_list[1]-1, calendar.monthrange(time_list[0], time_list[1]-1)[1])
+        elif len(time_list) == 1:
+            end = datetime.date(time_list[0]-1, 12, 31)
+    else:
+        end = datetime.date(2100, 1, 1)
+
+    return start, end
+
+
 def get_pool_data(project_name, timeframe):
     helpers_path = str(pathlib.Path(__file__).parent.parent.resolve()) + '/helpers'
+
+    start = get_start_date(timeframe)
+    end = get_timeframe_end(timeframe)
 
     pool_links = {}
 
     with open(helpers_path + f'/pool_information/{project_name}.json') as f:
         pool_data = json.load(f)
-        clusters = {}
-        if 'all' in pool_data['clusters']:
-            clusters.update(pool_data['clusters']['all'])
-        if timeframe[:YEAR_DIGITS] in pool_data['clusters']:
-            clusters.update(pool_data['clusters'][timeframe[:YEAR_DIGITS]])
-        for cluster, pools in clusters.items():
-            for (pool, _) in pools:
-                pool_links[pool] = cluster
-
+        cluster_data = pool_data['clusters']
     with open(helpers_path + '/legal_links.json') as f:
-        legal_links = json.load(f)
-        try:
-            for cluster, pools in legal_links[timeframe[:YEAR_DIGITS]].items():
-                for (pool, _) in pools:
-                    pool_links[pool] = cluster
-        except KeyError:
-            pass
+        legal_data = json.load(f)
+    for data in [cluster_data, legal_data]:
+        for cluster_name, pools in data.items():
+            for pool_info in pools:
+                link_start, link_end = get_time_period(pool_info['from'], pool_info['to'])
+
+                check_list = [  # Check if two periods overlap at any point
+                    start <= link_start <= end,
+                    start <= link_end <= end,
+                    link_start <= start <= link_end,
+                    link_start <= end <= link_end
+                ]
+                if any(check_list):
+                    pool_links[pool_info['name']] = cluster_name
 
     for parent, child in pool_links.items():  # resolve chain links
         while child in pool_links.keys():
@@ -45,6 +91,33 @@ def get_pool_data(project_name, timeframe):
         pool_links[parent] = child
 
     return pool_data, pool_links
+
+
+def get_pool_addresses(project_name, timeframe):
+    helpers_path = str(pathlib.Path(__file__).parent.parent.resolve()) + '/helpers'
+
+    start = get_start_date(timeframe)
+    end = get_timeframe_end(timeframe)
+
+    with open(helpers_path + f'/pool_information/{project_name}.json') as f:
+        address_data = json.load(f)['pool_addresses']
+
+    if not address_data:
+        return {}
+
+    address_links = {}
+    for address, addr_info in address_data.items():
+        link_start, link_end = get_time_period(addr_info['from'], addr_info['to'])
+
+        check_list = [  # Check if two periods overlap at any point
+            start <= link_start <= end,
+            start <= link_end <= end,
+            link_start <= start <= link_end,
+            link_start <= end <= link_end
+        ]
+        if any(check_list):
+            address_links[address] = addr_info['name']
+    return address_links
 
 
 def write_csv_file(project_dir, blocks_per_entity, timeframe):
