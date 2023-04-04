76b6c08c42a276ecbff46f9f4be129addbb8f7fe
LadyChristina <christina_ovezik@outlook.com>
2023-04-04 12:37:54 +0100

    Make query script more verbose

--- src/query.py
+++ src/query.py
@@ -10,26 +10,45 @@
 import google.cloud.bigquery as bq
 import json
 from yaml import safe_load
+from time import time
 
 from helpers.helper import ROOT_DIR, INPUT_DIR
+
+if not INPUT_DIR.is_dir():
+    INPUT_DIR.mkdir()
 
 with open(ROOT_DIR / "queries.yaml") as f:
     queries = safe_load(f)
 
 client = bq.Client.from_service_account_json(json_credentials_path=ROOT_DIR / "google-service-account-key.json")
+
+force_query = False
 for ledger in queries.keys():
-    print(f"Querying {ledger}...")
+    filename = f'{ledger}_raw_data.json'
+    file = INPUT_DIR / filename
+    if not force_query and file.is_file():
+        continue
+    print(f"Querying {ledger}..")
+    start = time()
     QUERY = (queries[ledger])
     query_job = client.query(QUERY)
     try:
         rows = query_job.result()
+        print(f'Done querying {ledger} (took about {round(time() - start)} seconds)')
     except Exception as e:
         print(f'{ledger} query failed, please make sure it is properly defined.')
         print(f'The following exception was raised: {repr(e)}')
         continue
 
+    print(f"Formatting {ledger} data..")
+    start = time()
     result_json = [dict(row) for row in rows]
+    print(f'Done formatting {ledger} data (took about {round(time() - start)} seconds)')
 
-    filename = f'{ledger}_raw_data.json'
-    with open(INPUT_DIR / filename, 'w') as f:
+    print(f"Writing {ledger} data to file..")
+    start = time()
+    with open(file, 'w') as f:
         json.dump(result_json, f, default=str)
+    print(f'Done writing {ledger} data (took about {round(time() - start)} seconds)')
+    print('-------------------------------------------------------')
+
