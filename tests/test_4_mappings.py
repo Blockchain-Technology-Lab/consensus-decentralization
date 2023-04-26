477137cfba94d829298df0afb40a4b38563320c0
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-04-26 10:12:57 +0100

    Fix eth mapping test with known address

--- tests/test_4_mappings.py
+++ tests/test_4_mappings.py
@@ -149,7 +149,7 @@ def test_ethereum_mapping():
 
     expected_output = [
         'Entity,Resources\n',
-        '0x3bee5122e2a2fbe11287aafb0cb918e22abb5436,1'
+        'MEV Builder: 0x3B...436,1'
     ]
 
     output_file = OUTPUT_DIR / project / f'{timeframes[0]}.csv'
